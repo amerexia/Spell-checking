bbef54bbb1596d48d4a2f7fa7dbdf1efa730fc51
Dounia Belannab <dbelannab@gmail.com>
2022-12-09 19:58:47 +0100

    Initial Commit

---
+++ SpellChecking/SpellCheckingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpellCheckingApp.swift
+//  SpellChecking
+//
+//  Created by Dounia Belannab on 09/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct SpellCheckingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
